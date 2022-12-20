bee787370b00b470b413b58188eade08beb40451
w0oodpecker <vladislav.sizov@rambler.ru>
2022-12-20 20:35:05 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class PhoneBook {
+
+    public HashMap<String, String> collersMap = new HashMap<>();
+
+    public boolean add(String nameOfCaller0, String numberOfCaller0){
+        return false;
+    }
+
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,26 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class PhoneBookTest {
+
+
+    @Test
+    public void testAdd() {
+        String nameOfCaller0 = "Иванов Иван";
+        String numberOfCaller0 = "+79991111111";
+        String nameOfCaller1 = "Сидоров Сидр";
+        String numberOfCaller1 = "+79992222222";
+
+        //assert
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add(nameOfCaller0, numberOfCaller0);
+        phoneBook.add(nameOfCaller1, numberOfCaller1);
+
+        //act
+        Assertions.assertEquals(phoneBook.collersMap.get(nameOfCaller0), numberOfCaller0);
+        Assertions.assertEquals(phoneBook.collersMap.get(nameOfCaller1), numberOfCaller1);
+    }
+
 }
