be9a450a29b3488d114d90f3a4680b3de6086963
w0oodpecker <vladislav.sizov@rambler.ru>
2022-12-21 20:37:37 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,15 +1,15 @@
 package org.example;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class PhoneBook {
 
     public HashMap<String, String> callersMap = new HashMap<>();
 
-    public boolean add(String nameOfCaller0, String numberOfCaller0){
+    public boolean add(String nameOfCaller, String numberOfCaller){
+        if(callersMap.putIfAbsent(nameOfCaller, numberOfCaller) == null){
+            return true;
+        }
         return false;
     }
-
-
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -12,15 +12,20 @@ public class PhoneBookTest {
         String numberOfCaller0 = "+79991111111";
         String nameOfCaller1 = "Сидоров Сидр";
         String numberOfCaller1 = "+79992222222";
+        String nameOfCaller2 = "Петров Пётр";
+        String numberOfCaller2 = "+7999333333";
 
         //assert
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add(nameOfCaller0, numberOfCaller0);
         phoneBook.add(nameOfCaller1, numberOfCaller1);
 
+
         //act
-        Assertions.assertEquals(numberOfCaller0, phoneBook.callersMap.get(nameOfCaller0));
-        Assertions.assertEquals(numberOfCaller1, phoneBook.callersMap.get(nameOfCaller1));
+        Assertions.assertEquals(numberOfCaller0, phoneBook.callersMap.get(nameOfCaller0)); //Проверка добавления
+        Assertions.assertEquals(numberOfCaller1, phoneBook.callersMap.get(nameOfCaller1)); //Проверка добавления
+        Assertions.assertEquals(false, phoneBook.add(nameOfCaller1, numberOfCaller1)); //Проверка на дублирование
+        Assertions.assertEquals(true, phoneBook.add(nameOfCaller2, numberOfCaller2)); //Проверка на отсутствие дублирования
     }
 
 }
